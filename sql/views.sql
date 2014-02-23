f22cdb79ab832ba191ea7d5c7b06ce1336be577c
Zen Desk <zenwong@gmail.com>
2014-02-23 11:06:08 +0800

    untrack some files and directories

--- sql/views.sql
+++
@@ -1,9 +0,0 @@
-select
-vids.title as title,
-vids.path as path,
-tags.name as tag
-from vids
-join tags on vidtags.tid = tags._id
-join vidtags on vidtags.title = vids.title
-and tag in('rape', 'incest')
-group by vids.title having count(vids.title) = 2;
